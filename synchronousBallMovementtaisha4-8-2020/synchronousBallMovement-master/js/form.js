f510a68929a6a9d01864020603e5c527fac351ba
taishakarsiya <66948929+taishakarsiya@users.noreply.github.com>
2020-08-04 17:06:07 +0530

    Add files via upload

---
+++ synchronousBallMovementtaisha4-8-2020/synchronousBallMovement-master/js/form.js
@@ -0,0 +1,27 @@
+class Form {
+constructor(){
+
+
+}
+
+display(){
+var title = createElement('h2');
+title.html("Car Racing Game")
+title.position(130,0);
+var input = createInput("name")
+input.position(130,160);
+var button = createButton("play");
+button.position(250,200);
+button.mousePressed(function (){
+button.hide();
+ input.hide(); 
+var name = input.value();
+playerCount = playerCount + 1;
+player.update(name);
+player.updateCount(playerCount);
+var greeting= createElement('h2');
+greeting.html("hello"+name)
+greeting.position(130,160);
+});
+}
+}
